1911b01b61e61a72ddacd4c3237b92f4f79e5e96
alfaiz khan <alfaizk64@gmail.com>
2025-01-04 22:06:26 +0530

    changes in home hoe form get submit using javascript just to take response from api weather it is success of error so we can show elert for success or fail and pdf also getting uploaded

--- middlewares/loginAuth.js
+++ middlewares/loginAuth.js
@@ -15,31 +15,5 @@ const loginauth = (req, res) => {
     res.render('login');
   }
 
-//   const registerauth = (req, res) => {
-//     const token = req.cookies.token;
-//     if (token) {
-//       try {
-//         jwt.verify(token, secretKey);
-//         return res.redirect('/home');
-//       } catch (err) {
-//         // Token is invalid or expired
-//         return res.clearCookie("token").render("register");
-//       }
-//     }
-//     res.render('register');
-//   }
 
-//   const homeauth = (req, res) => {
-//     const token = req.cookies.token;
-//     if (!token) {
-//       return res.redirect('/login');
-//     }
-//     try {
-//       jwt.verify(token, secretKey);
-//       return res.render('home');
-//     } catch (err) {
-//       // Token is invalid or expired
-//       return res.clearCookie("token").redirect('/login');
-//     }
-//   }
 module.exports =loginauth
